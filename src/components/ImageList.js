6bd7f6386a613cd81b4f030f861932af7c455bbf
James <james.lee4789@gmail.com>
2019-06-17 23:31:07 -0400

    add image card so that we can style its own card

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
+import ImageCard from './ImageCard';
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map(({description, id, urls}) => {
-    return <img alt={description} key={id} src={urls.regular}/>
-  })
+  const images = props.images.map(image => {
+    return <ImageCard image={image} key={image.id} />;
+  });
 
-  return <div>{images}</div>
+  return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
